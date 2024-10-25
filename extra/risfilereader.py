d1c4c2739d9b59d4ef88e6ba238b547c5cd06ebb
ravinder singh <ravi199777@gmail.com>
2024-10-25 15:40:54 +0530

    risfile reading complete. integeration left

--- extra/risfilereader.py
+++ extra/risfilereader.py
@@ -20,10 +20,39 @@ with open(filepath, 'r') as bibliography_file:
 
         journal_abrevated = entry.get('alternate_title1',None)
         publication_date = entry.get('date',None)
+        
+        start_page = entry.get('start_page',None)
+        end_page = entry.get('end_page',None)
+        pages = None
+        if start_page is not None and end_page is not None:
+            pages =  f'{start_page}-{end_page}'
 
+        journal_volume = entry.get('volume',None)
+        journal_issue = entry.get('number',None)
+        
+        pubmed_id = entry.get('pubmed_id',None)
+        pmc_id = entry.get('pmc_id',None)
+        pii = entry.get('pii',None)
+        doi = entry.get('doi',None)
+        print_issn = entry.get('print_issn',None) or entry.get('issn',None)
+        electronic_issn = entry.get('electronic_issn',None)
+        linking_issn = entry.get('linking_issn',None)
+        nlm_journal_id = entry.get('nlm_journal_id',None)
+    
+    
+        file_attachments1 = entry.get('file_attachments1',None)
+        file_attachments2 = entry.get('file_attachments2',None)
+        urls = entry.get('urls',None)
+    
+        links = []
+        
+        if file_attachments1 is not None:
+            links.append(file_attachments1)
 
+        if file_attachments2 is not None:
+            links.append(file_attachments2)
 
-
+        links += (urls or [])
         if entry['type_of_reference'] == 'JOUR' and author in authors and journal is not None:
             print('keywords : ',keywords)
             print('authors : ',authors)
@@ -31,16 +60,19 @@ with open(filepath, 'r') as bibliography_file:
             print('abstract : ',abstract)
             print('place_of_publication : ',place_of_publication)
             print('journal : ',journal)
-            print('alternate_title1 : ',journal_abrevated)
+            print('journal_abrevated : ',journal_abrevated)
             print('publication_date : ',publication_date)
-            # print('start_page : ',entry.get('start_page',None))
-            # print('end_page : ',entry.get('end_page',None))
-            # print('volume : ',entry.get('volume',None))
-            # print('number : ',entry.get('number',None))
-            # print('doi : ',entry.get('doi',None))
-            # print('issn : ',entry.get('issn',None))
-            # print('file_attachments1 : ',entry.get('file_attachments1',None))
-            # print('file_attachments2 : ',entry.get('file_attachments2',None))
-            # print('urls : ',entry.get('urls',None))
+            print('pages : ',pages)
+            print('journal_volume : ',journal_volume)
+            print('number : ',journal_issue)
+            print('pubmed_id : ',pubmed_id)
+            print('pmc_id : ',pmc_id)
+            print('pii : ',pii)
+            print('doi : ',doi)
+            print('print_issn : ',print_issn)
+            print('electronic_issn : ',electronic_issn)
+            print('linking_issn : ',linking_issn)
+            print('nlm_journal_id : ',nlm_journal_id)
+            print('links : ',links)
             
             print('\n')
